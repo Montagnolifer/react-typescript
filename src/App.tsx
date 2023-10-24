3e68186a2326120eaec1917cae9fbe4f508f6aef
Montagnoli1999 <montagnoli@dallascorporation.com.br>
2023-10-24 15:05:16 -0300

    Projeto Book objetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-import './App.css'
+import Book from "./components/book";
+import "./App.css";
+import { book1, book2, book3 } from "./data";
 
 function App() {
 
-  const cidade = 'Birigui';
-  const atletas = ["Siban", "Renato Cariane", "Alex dinossauro", "João Alberto", "Mariana"];
-  const processadores = [
-    {
-      id: 1,
-      title: "i5",
-      preco: 3000,
-    },
-    {
-      id: 2,
-      title: "i7",
-      preco: 6000,
-    },
-    {
-      id: 3,
-      title: "i9",
-      preco: 9000,
-    },
-  ]
   return (
     <>
-      <h1>Minha cidade é: {cidade}</h1>
-
-      {atletas.map((e) => ( 
-        <h2>{e}</h2>
-      ))}
-
-      {
-        processadores.map((e) => (
-          <div>
-            <h2>{e.title}</h2>
-            <h3>{e.preco}</h3>
-          </div>
-        ))
-      }
+      <Book livro={book1} />
+      <Book livro={book2} />
+      <Book livro={book3} />
     </>
   )
 }
